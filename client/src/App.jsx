744f6f2fb31d00f51650cf5b0ddeffa0f37c19a7
Muhammad-Haram <haramh643@gmail.com>
2024-09-23 17:37:05 +0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
@@ -5,8 +7,43 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
+
+const user = false;
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/products/:category",
+    element: <ProductList />,
+  },
+  {
+    path: "/product/:id",
+    element: <Product />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  }
+]);
+
 const App = () => {
-  return <Home/>;
+
+
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  );
 };
 
 export default App;
